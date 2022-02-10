c25ba7fab916f8b83cf2eb6adf88403e313fe925
Rob Desilets <rob@churchcasting.io>
2022-02-10 14:58:22 -0600

    Add Ability to set custom key length in config

--- src/Helper.php
+++ src/Helper.php
@@ -55,7 +55,7 @@ class Helper
     {
 
         $key = openssl_pkey_new([
-            'private_key_bits' => 4096,
+            'private_key_bits' => 2048,
             'private_key_type' => OPENSSL_KEYTYPE_RSA,
         ]);
         openssl_pkey_export($key, $pem);
