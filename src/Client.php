7201420b8be94bd0ad4ea5c86d208b53ef6351e7
AlexNodex <nodex@icloud.com>
2024-12-09 13:55:35 +0000

    Update Client.php
    Fix for lets encrypt's removal of initialIp from the json response.
    
    https://community.letsencrypt.org/t/has-the-output-of-initialip-changed-in-the-account-directory/229823/7

--- src/Client.php
+++ src/Client.php
@@ -357,7 +357,7 @@ class Client
         $data = json_decode((string)$response->getBody(), true);
         $accountURL = $response->getHeaderLine('Location');
         $date = (new \DateTime())->setTimestamp(strtotime($data['createdAt']));
-        return new Account($data['contact'], $date, ($data['status'] == 'valid'), $data['initialIp'], $accountURL);
+        return new Account($data['contact'], $date, ($data['status'] == 'valid'), $accountURL);
     }
 
     /**
